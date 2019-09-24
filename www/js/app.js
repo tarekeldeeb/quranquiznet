5c870b73fa4229bec4d9ca7830e8fbf25f16b1fc
Tarek Eldeeb <tarekeldeeb@msn.com>
2019-09-24 13:39:13 +0200

    Update Whatsapp lib share URL.

--- www/js/app.js
+++ www/js/app.js
@@ -90,7 +90,7 @@ angular.module('quranquiznet', ['ionic', 'ui.router', 'quranquiznet.controllers'
   $rootScope.auth = firebase.auth();
   $rootScope.database = firebase.database();
   $rootScope.storage = firebase.storage();
-  $rootScope.source_version = "185";
+  $rootScope.source_version = "186";
   $rootScope.appName = "اختبار القرآن";
   $rootScope.Loc = {};
     
