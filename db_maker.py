ae7535e750c539e29eb0a15a26152c1ae0d2509b
Tarek ELDEEB <tarek.eldeeb@valeo.com>
2021-06-23 16:37:17 +0200

    Added sim2idx and sim3idx columns to q.json

--- db_maker.py
+++ db_maker.py
@@ -7,15 +7,14 @@
 """
 import requests
 import re
-from time import sleep
 from tqdm import tqdm
 
 uthmani_text: str = "quran-uthmani-min.txt"
 
 
-def removeDialects(s):
-  nodial = re.sub("[" + chr(0x64B) + "-" + chr(0x65E) + chr(0x670) + "ۦ۟ـۧ" + "]", "", s)
-  return re.sub("[" + chr(0x622) + chr(0x623) + chr(0x625) + "]", chr(0x627), nodial)
+def remove_dialects(s):
+  no_dialects = re.sub("[" + chr(0x64B) + "-" + chr(0x65E) + chr(0x670) + "ۦ۟ـۧ" + "]", "", s)
+  return re.sub("[" + chr(0x622) + chr(0x623) + chr(0x625) + "]", chr(0x627), no_dialects)
 
 
 if __name__ == "__main__":
@@ -32,14 +31,14 @@ if __name__ == "__main__":
   with open(uthmani_text, encoding="utf8") as f:
     for line in f:
       tokens = line.strip().split('|')
-      if (len(tokens) == 3):
+      if len(tokens) == 3:
         aya = tokens[2].split(" ")
         aya_dict[aya_idx + len(aya) - 1] = tokens[1]
         aya_idx += len(aya)
         text_dial.extend(aya)
-  text_full = removeDialects(" ".join(text_dial))
-  text_nodial = text_full.split(" ")
-  print("Found words: " + str(len(text_nodial)))
+  text_full = remove_dialects(" ".join(text_dial))
+  text_no_dialect = text_full.split(" ")
+  print("Found words: " + str(len(text_no_dialect)))
   open("2-" + uthmani_text, "w", encoding='utf-8').write(text_full)
   json_head = '{"type":"database","name":"qq-noIdx",' \
               '"objects":[{"type":"table","name":"q","ddl":"CREATE TABLE \\"q\\" ' \
@@ -49,34 +48,36 @@ if __name__ == "__main__":
               '\\"sim1\\" INTEGER NOT NULL DEFAULT (1),' \
               '\\"sim2\\" INTEGER NOT NULL DEFAULT (0),' \
               '\\"sim3\\" INTEGER NOT NULL DEFAULT (0),' \
+              '\\"sim2idx\\" TEXT NULL,' \
+              '\\"sim3idx\\" TEXT NULL,' \
               '\\"aya\\" INTEGER DEFAULT(NULL))",' \
               '"rows":['
   json_tail = ']}]}'
   jf = open("q.json", "w", encoding='utf-8')
   jf.write(json_head)
-  for i in tqdm(range(len(text_nodial) - 2)):
-    row = "[" + str(i + 1) + ",\"" + text_nodial[i] + "\",\"" + text_dial[i] + "\","
+  for i in tqdm(range(len(text_no_dialect) - 2)):
+    row = "[" + str(i + 1) + ",\"" + text_no_dialect[i] + "\",\"" + text_dial[i] + "\","
     sim1 = 0
     sim2 = 0
     sim3 = 0
-    for j in [x for x in range(len(text_nodial) - 2) if x != i]:
-      if text_nodial[i] == text_nodial[j]:
+    sim2idx = "["
+    sim3idx = "["
+    for j in [x for x in range(len(text_no_dialect) - 2) if x != i]:
+      if text_no_dialect[i] == text_no_dialect[j]:
         sim1 += 1
-        if text_nodial[i + 1] == text_nodial[j + 1]:
+        if text_no_dialect[i + 1] == text_no_dialect[j + 1]:
           sim2 += 1
-          if text_nodial[i + 2] == text_nodial[j + 2]:
+          sim2idx += str(j) + ","
+          if text_no_dialect[i + 2] == text_no_dialect[j + 2]:
             sim3 += 1
-    if i in aya_dict:
-      aya_cnt = str(aya_dict[i])
-    else:
-      aya_cnt = "null"
+            sim3idx += str(j) + ","
+    sim2idx = "null" if len(sim2idx) == 1 else "\"" + sim2idx[:-1] + "]\""
+    sim3idx = "null" if len(sim3idx) == 1 else "\"" + sim3idx[:-1] + "]\""
+    aya_cnt = str(aya_dict[i]) if i in aya_dict else "null"
+    comma = "" if i == len(text_no_dialect) - 3 else ","
 
-    if i == len(text_nodial) - 3:
-      comma = ""
-    else:
-      comma = ","
-    row += str(sim1) + "," + str(sim2) + "," + str(sim3) + "," + aya_cnt + "]" + comma
-    # TODO: Aya
+    row += str(sim1) + "," + str(sim2) + "," + str(sim3) + "," \
+           + sim2idx + "," + sim3idx + "," + aya_cnt + "]" + comma
     jf.write(row)
   jf.write(json_tail)
   jf.close()
